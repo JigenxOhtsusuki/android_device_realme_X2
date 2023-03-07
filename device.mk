0131449cd1df6de5e414a8f176186c7eb2c629c4
Retypolkg <retypolkg2021@gmail.com>
2023-03-07 09:15:44 +0100

    X2: Initial AOSPA bringup

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
